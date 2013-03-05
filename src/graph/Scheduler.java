267e396a1ae184933da147a0fed226b3878fe37c
jeselfalan <jeselfalan@gmail.com>
2013-03-04 15:22:14 -1000

    first commit. added file reader to main

--- src/graph/Scheduler.java
+++ src/graph/Scheduler.java
@@ -1,7 +1,11 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-
 public class Scheduler{
 
 	/**
@@ -9,7 +13,32 @@ public class Scheduler{
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+			
+		 try{
+			 //retrieve the file path arguments 
+			 if(args.length != 1) {
+				  System.err.println("Invalid command line, exactly one argument required");
+				  System.exit(1);
+				}
+				FileInputStream fstream = new FileInputStream(args[0]); 
+	        
+	         	// Get the object of DataInputStream
+	            DataInputStream in = new DataInputStream(fstream);
+	                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	            String strLine;
+	            //Read File Line By Line
+	            while ((strLine = br.readLine()) != null)   {
+	              // Print the content on the console
+	          
+	            	System.out.println (strLine);
+	            }
+	            //Close the input stream
+	            in.close();
+	            }catch (Exception e){//Catch exception if any
+	              System.err.println("Error: " + e.getMessage());
+	            }
+    
+	
 		ArrayList<Vertex> Vertices = new ArrayList<Vertex>();
 		
 		//ThemeParkGraph graph;
@@ -25,5 +54,6 @@ public class Scheduler{
 		System.out.println("total vertex count: " + Vertices.size());	
 		
 	}
-
+	
+	
 }
