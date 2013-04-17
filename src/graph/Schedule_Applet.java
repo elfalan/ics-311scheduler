3c6a35bf3365eb8d087aebdf20c899c2cb7b9be0
jesseelfalan <jesseelfalan@192.168.1.143>
2013-04-16 22:44:40 -1000

    added schedule applet class

---
+++ src/graph/Schedule_Applet.java
@@ -0,0 +1,17 @@
+package graph;
+
+import java.awt.Graphics;
+
+import javax.swing.JApplet;
+import javax.swing.JTextArea;
+
+
+public class Schedule_Applet extends JApplet {
+
+	@Override
+    public void init() {
+        JTextArea area = new JTextArea("Hello World");
+        this.add(area);
+    }
+	
+}
